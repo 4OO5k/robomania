08f3da408551d5f93006348cf81f8a426257b7e9
mickeycookie <richardson0705@yahoo.com>
2024-11-26 17:47:59 -0500

    .

---
+++ DR - Robomania/Assets/Scripts/Crusher1.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crusher1 : MonoBehaviour
+{
+    public float speed;
+
+    private void FixedUpdate()
+    {
+        if (transform.position.x <= -8 || transform.position.x >= 8)
+        {
+            speed *= -1;
+        }
+        float newXPosition = transform.position.x + speed * Time.fixedDeltaTime;
+        float newYPosition = transform.position.y;
+        Vector2 newPosition = new Vector2(newXPosition, newYPosition);
+        transform.position = newPosition;
+    }
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().AddForce(Vector3.left * speed, ForceMode2D.Impulse);
+        GetComponent<Rigidbody2D>().AddForce(Vector3.right * speed, ForceMode2D.Impulse);
+    }
+}
